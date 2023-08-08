73b1b342986bab432d5d1ca1f26eb7987f7aeeb0
vlorian <vlorian.dervishi@gmail.com>
2023-08-08 16:39:03 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(s = "||") {
+  return function (value = "*") {
+    return `You are ${s}${value}${s}!`;
+  };
+}
